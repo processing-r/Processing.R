6348bd43ebc36752c4f044ca669d4bc8bd554cc8
Ce Gao <ce.gao@outlook.com>
2016-08-29 11:45:51 +0800

    update the example about draw function

--- example/test-draw.R
+++ example/test-draw.R
@@ -1,9 +1,10 @@
 yPos <- 0.0;
  
 draw <- function() {
+  processing$background(as.integer(204))
   yPos <- yPos - 1.0;
   if (yPos < 0) {
-    yPos <- 100;
+    yPos <- height;
   }
-  processing$line(0, yPos, width, yPos);
+  processing$line(0, yPos, width, yPos)
 }
