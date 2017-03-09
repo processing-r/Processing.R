9a3ac348f0c6f23d54d7256a2f9941a53a5063af
Ce Gao <ce.gao@outlook.com>
2017-03-09 11:57:03 +0800

    Docker: Add Docker support
    Signed-off-by: Ce Gao <ce.gao@outlook.com>

---
+++ .docker/generate-ant-file-in-docker.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# init.sh establishes a initial environment to build Processing.R in travis.
+# Most of the code is copied from `scripts/generate-ant-file.sh`
+
+# Timestamped log, e.g. log "cluster created".
+#
+# Input:
+#   $1 Log string.
+function log {
+  echo -e "[`TZ=Asia/Shanghai date`] ${1}"
+}
+
+root=$(dirname "${BASH_SOURCE}")/..
+
+# Paths
+# Those paths are not needed when building runner.jar,
+# but will be used in the future to package the mode.
+modes="/mock-user/modes"
+executable="/mock-user/Processing"
+# Those paths are important to build runner.jar.
+processing="/code/processing"
+core="/code/processing/core/library/"
+pde="/code/processing/lib/"
+renjin="lib/renjin-script-engine-0.8.2194-jar-with-dependencies.jar"
+
+cd ${root}
+cp build.xml.template build.xml
+# Interpret config template.
+log "Inject the config to build.xml.template"
+perl -i -pe "s|\@\@modes\@\@|${modes}|g" build.xml
+perl -i -pe "s|\@\@executable\@\@|${executable}|g" build.xml
+perl -i -pe "s|\@\@processing\@\@|${processing}|g" build.xml
+perl -i -pe "s|\@\@core\@\@|${core}|g" build.xml
+perl -i -pe "s|\@\@pde\@\@|${pde}|g" build.xml
+perl -i -pe "s|\@\@renjin\@\@|${renjin}|g" build.xml
+cd - > /dev/null
