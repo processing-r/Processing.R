d5a8b083155a6e84e6218a16f67d5af2bbca0645
Ce Gao <ce.gao@outlook.com>
2017-05-06 15:49:49 +0800

    builtin: Add 2d built-in functions
    Signed-off-by: Ce Gao <ce.gao@outlook.com>

--- src/rprocessing/applet/BuiltinApplet.java
+++ src/rprocessing/applet/BuiltinApplet.java
@@ -26,4 +26,28 @@ public class BuiltinApplet extends PApplet {
     public void ellipse(double posX, double posY, double width, double height) {
     	super.ellipse((float) posX, (float) posY, (float) width, (float) height);
     }
+    
+    public void arc(double posX, double posY, double width, double height, double start, double stop) {
+    	super.arc((float) posX, (float) posY, (float) width, (float) height, (float) start, (float) stop);
+    }
+    
+    public void quad(double posAX, double posAY, double posBX, double posBY, double posCX, double posCY, double posDX, double posDY) {
+    	super.quad((float) posAX, (float) posAY, (float) posBX, (float) posBY, (float) posCX, (float) posCY, (float) posDX, (float) posDY);
+    }
+    
+    public void rect(double posAX, double posAY, double posBX, double posBY) {
+    	super.rect((float) posAX, (float) posAY, (float) posBX, (float) posBY);
+    }
+    
+    public void rect(double posAX, double posAY, double posBX, double posBY, double redii) {
+    	super.rect((float) posAX, (float) posAY, (float) posBX, (float) posBY, (float) redii);
+    }
+    
+    public void rect(double posAX, double posAY, double posBX, double posBY, double tl, double tr, double br, double bl) {
+    	super.rect((float) posAX, (float) posAY, (float) posBX, (float) posBY, (float) tl, (float) tr, (float) br, (float) bl);
+    }
+    
+    public void triangle(double posAX, double posAY, double posBX, double posBY, double posCX, double posCY) {
+    	super.triangle((float) posAX, (float) posAY, (float) posBX, (float) posBY, (float) posCX, (float) posCY);
+    }
 }
