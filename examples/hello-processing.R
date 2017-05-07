cae9fca4596fd78d31f63e2419be741aefd19264
Ce Gao <ce.gao@outlook.com>
2017-05-07 11:29:40 +0800

    builtin: Add curves function
    Signed-off-by: Ce Gao <ce.gao@outlook.com>

--- examples/hello-processing.R
+++ examples/hello-processing.R
@@ -1,6 +1,36 @@
 processing$line(11, 22, 33, 22)
+
 processing$arc(50, 55, 50, 50, 0, 100)
+
 processing$triangle(30, 75, 58, 20, 86, 75)
+
 processing$ellipse(56, 46, 55, 55)
+
 processing$quad(38, 31, 86, 20, 69, 63, 30, 76)
-print("Hello, Processing.R")
+
+processing$bezierDetail(as.integer(12))
+
+bezier(85, 20, 10, 10, 90, 90, 15, 80)
+
+# for (i in 1:16) {
+#   t = i / float(steps);
+#   x = processing$bezierPoint(85, 10, 90, 15, t);
+#   y = processing$bezierPoint(20, 10, 90, 80, t);
+#   tx = processing$bezierTangent(85, 10, 90, 15, t);
+#   ty = processing$bezierTangent(20, 10, 90, 80, t);
+#   a = atan2(ty, tx);
+#   a -= HALF_PI;
+#   line(x, y, cos(a)*8 + x, sin(a)*8 + y);
+# }
+
+# bezierPoint
+for (i in 1:10) {
+  t = i / 10
+  x = processing$bezierPoint(85, 10, 90, 15, t)
+  y = processing$bezierPoint(20, 10, 90, 80, t)
+  processing$ellipse(x, y, 5, 5)
+}
+
+processing$curve(5, 26, 73, 24, 73, 61, 15, 65)
+
+stdout$print("Hello, Processing.R")
--- src/rprocessing/applet/BuiltinApplet.java
+++ src/rprocessing/applet/BuiltinApplet.java
@@ -3,7 +3,7 @@ package rprocessing.applet;
 import processing.core.PApplet;
 
 public class BuiltinApplet extends PApplet {
-	
+
     /*
      * Wrapper functions
      * 
@@ -15,6 +15,10 @@ public class BuiltinApplet extends PApplet {
         super.size((int) width, (int) height);
     }
 
+    /*
+     * 2D Primitives
+     */
+
     public void point(double x, double y) {
         super.point((float) x, (float) y);
     }
@@ -22,32 +26,96 @@ public class BuiltinApplet extends PApplet {
     public void line(double posAX, double posAY, double posBX, double posBY) {
         super.line((float) posAX, (float) posAY, (float) posBX, (float) posBY);
     }
-    
+
     public void ellipse(double posX, double posY, double width, double height) {
-    	super.ellipse((float) posX, (float) posY, (float) width, (float) height);
+        super.ellipse((float) posX, (float) posY, (float) width, (float) height);
     }
-    
-    public void arc(double posX, double posY, double width, double height, double start, double stop) {
-    	super.arc((float) posX, (float) posY, (float) width, (float) height, (float) start, (float) stop);
+
+    public void arc(double posX, double posY, double width, double height, double start,
+                    double stop) {
+        super.arc((float) posX, (float) posY, (float) width, (float) height, (float) start,
+            (float) stop);
     }
-    
-    public void quad(double posAX, double posAY, double posBX, double posBY, double posCX, double posCY, double posDX, double posDY) {
-    	super.quad((float) posAX, (float) posAY, (float) posBX, (float) posBY, (float) posCX, (float) posCY, (float) posDX, (float) posDY);
+
+    public void quad(double posAX, double posAY, double posBX, double posBY, double posCX,
+                     double posCY, double posDX, double posDY) {
+        super.quad((float) posAX, (float) posAY, (float) posBX, (float) posBY, (float) posCX,
+            (float) posCY, (float) posDX, (float) posDY);
     }
-    
+
     public void rect(double posAX, double posAY, double posBX, double posBY) {
-    	super.rect((float) posAX, (float) posAY, (float) posBX, (float) posBY);
+        super.rect((float) posAX, (float) posAY, (float) posBX, (float) posBY);
     }
-    
+
     public void rect(double posAX, double posAY, double posBX, double posBY, double redii) {
-    	super.rect((float) posAX, (float) posAY, (float) posBX, (float) posBY, (float) redii);
+        super.rect((float) posAX, (float) posAY, (float) posBX, (float) posBY, (float) redii);
     }
-    
-    public void rect(double posAX, double posAY, double posBX, double posBY, double tl, double tr, double br, double bl) {
-    	super.rect((float) posAX, (float) posAY, (float) posBX, (float) posBY, (float) tl, (float) tr, (float) br, (float) bl);
+
+    public void rect(double posAX, double posAY, double posBX, double posBY, double tl, double tr,
+                     double br, double bl) {
+        super.rect((float) posAX, (float) posAY, (float) posBX, (float) posBY, (float) tl,
+            (float) tr, (float) br, (float) bl);
     }
-    
-    public void triangle(double posAX, double posAY, double posBX, double posBY, double posCX, double posCY) {
-    	super.triangle((float) posAX, (float) posAY, (float) posBX, (float) posBY, (float) posCX, (float) posCY);
+
+    public void triangle(double posAX, double posAY, double posBX, double posBY, double posCX,
+                         double posCY) {
+        super.triangle((float) posAX, (float) posAY, (float) posBX, (float) posBY, (float) posCX,
+            (float) posCY);
+    }
+
+    /*
+     * Curves
+     */
+
+    public void bezier(double posAX, double posAY, double posAZ, double posBX, double posBY,
+                       double posBZ, double posCX, double posCY, double posCZ, double posDX,
+                       double posDY, double posDZ) {
+        super.bezier((float) posAX, (float) posAY, (float) posAZ, (float) posBX, (float) posBY,
+            (float) posBZ, (float) posCX, (float) posCY, (float) posCZ, (float) posDX,
+            (float) posDY, (float) posDZ);
+    }
+
+    public void bezier(double posAX, double posAY, double posBX, double posBY, double posCX,
+                       double posCY, double posDX, double posDY) {
+        super.bezier((float) posAX, (float) posAY, (float) posBX, (float) posBY, (float) posCX,
+            (float) posCY, (float) posDX, (float) posDY);
+    }
+
+    public double bezierPoint(double posAX, double posAY, double posBX, double posBY, double t) {
+        return super.bezierPoint((float) posAX, (float) posAY, (float) posBX, (float) posBY,
+            (float) t);
+    }
+
+    public double bezierTangent(double posAX, double posAY, double posBX, double posBY, double t) {
+        return super.bezierTangent((float) posAX, (float) posAY, (float) posBX, (float) posBY,
+            (float) t);
+    }
+
+    public void curve(double posAX, double posAY, double posAZ, double posBX, double posBY,
+                      double posBZ, double posCX, double posCY, double posCZ, double posDX,
+                      double posDY, double posDZ) {
+        super.curve((float) posAX, (float) posAY, (float) posAZ, (float) posBX, (float) posBY,
+            (float) posBZ, (float) posCX, (float) posCY, (float) posCZ, (float) posDX,
+            (float) posDY, (float) posDZ);
+    }
+
+    public void curve(double posAX, double posAY, double posBX, double posBY, double posCX,
+                      double posCY, double posDX, double posDY) {
+        super.curve((float) posAX, (float) posAY, (float) posBX, (float) posBY, (float) posCX,
+            (float) posCY, (float) posDX, (float) posDY);
+    }
+
+    public double curvePoint(double posAX, double posAY, double posBX, double posBY, double t) {
+        return super.curvePoint((float) posAX, (float) posAY, (float) posBX, (float) posBY,
+            (float) t);
+    }
+
+    public double curveTangent(double posAX, double posAY, double posBX, double posBY, double t) {
+        return super.curveTangent((float) posAX, (float) posAY, (float) posBX, (float) posBY,
+            (float) t);
+    }
+
+    public void curveTightness(double tightness) {
+        super.curveTightness((float) tightness);
     }
 }
