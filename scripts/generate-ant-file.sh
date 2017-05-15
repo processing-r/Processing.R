ae6951b65a09dbe9b143c6b16ac9e0852ee5aa20
Ce Gao <ce.gao@outlook.com>
2017-05-15 13:32:07 +0800

    scripts: Update the dep about core.jar and pde.jar
    Closes https://github.com/gaocegege/Processing.R/issues/13 and https://github.com/gaocegege/Processing.R/issues/42
    
    Signed-off-by: Ce Gao <ce.gao@outlook.com>

--- scripts/generate-ant-file.sh
+++ scripts/generate-ant-file.sh
@@ -1,42 +1,12 @@
 #!/usr/bin/env bash
 
-# Timestamped log, e.g. log "cluster created".
-#
-# Input:
-#   $1 Log string.
-function log {
-  echo -e "[`TZ=Asia/Shanghai date`] ${1}"
-}
-
-root=$(dirname "${BASH_SOURCE}")/..
+source "$(dirname "${BASH_SOURCE}")/utils/generator-util.sh"
 
 # Path to be changed
 modes="/Users/gaoce/Documents/Processing/modes"
 executable="/Applications/Processing.app/Contents/MacOS/Processing"
-core="../processing/core/library/"
-pde="../processing/app/"
+core="/Applications/Processing.app/Contents/Java/core.jar"
+pde="/Applications/Processing.app/Contents/Java/pde.jar"
 
-# Static path
-renjin="lib/renjin-script-engine-0.8.2194-jar-with-dependencies.jar"
-
-# Build core.jar.
-cd ${core}/..
-ant build
-cd - > /dev/null
-
-# Build pde.jar.
-cd ${pde}
-ant build
-cd - > /dev/null
-
-cd ${root}
-cp build.xml.template build.xml
-# Interpret config template.
-log "Inject the config to build.xml.template"
-perl -i -pe "s|\@\@modes\@\@|${modes}|g" build.xml
-perl -i -pe "s|\@\@executable\@\@|${executable}|g" build.xml
-perl -i -pe "s|\@\@processing\@\@|${processing}|g" build.xml
-perl -i -pe "s|\@\@core\@\@|${core}|g" build.xml
-perl -i -pe "s|\@\@pde\@\@|${pde}|g" build.xml
-perl -i -pe "s|\@\@renjin\@\@|${renjin}|g" build.xml
-cd - > /dev/null
+# Call functions in utils/generator-util.sh
+generate-build-config ${modes} ${executable} ${core} ${pde}
