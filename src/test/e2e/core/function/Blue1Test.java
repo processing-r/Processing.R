0e438b3fffb7a7fc4c0321e274ea37f91f17a0bf
Ce Gao <ce.gao@outlook.com>
2017-08-08 14:05:36 +0800

    reference: Add brightness
    Signed-off-by: Ce Gao <ce.gao@outlook.com>

--- src/test/e2e/core/function/Blue1Test.java
+++ src/test/e2e/core/function/Blue1Test.java
@@ -9,7 +9,7 @@ import test.e2e.core.E2eTestBase;
 public class Blue1Test extends E2eTestBase {
 
   public Blue1Test() {
-    coreCodeTemplate = "c = color(175, 100, 220)  # Define color 'c'\nfill(c)  # Use color variable 'c' as fill color\nrect(15, 20, 35, 60)  # Draw left rectangle\n\nblueValue = blue(c)  # Get blue in 'c'\nprintln(blueValue)  # Prints \"220.0\"\nfill(0, 0, blueValue)  # Use 'blueValue' in new fill\nrect(50, 20, 35, 60)  # Draw right rectangle\n\nsaveFrame(\"%s\")\nexit()\n";
+    coreCodeTemplate = "c = color(175, 100, 220)  # Define color 'c'\nfill(c)  # Use color variable 'c' as fill color\nrect(15, 20, 35, 60)  # Draw left rectangle\n\nblueValue = blue(c)  # Get blue in 'c'\nprintln(blueValue)  # Prints '220.0'\nfill(0, 0, blueValue)  # Use 'blueValue' in new fill\nrect(50, 20, 35, 60)  # Draw right rectangle\n\nsaveFrame(\"%s\")\nexit()\n";
     referenceURI = "https://processing.org/reference/images/blue_.png";
   }
 
